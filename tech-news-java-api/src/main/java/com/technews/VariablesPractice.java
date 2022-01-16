478cc6f05938d4dde548962b1cbdf7ee079ac77b
ankurshahi80 <shahiankur80@gmail.com>
2022-01-16 13:37:01 -0500

    build database structure

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int x=1;
+        double y =1.5;
+        double  sum = x+y;
+        String z = "abc";
+        String t = z+x;
+        System.out.print(sum);
+        System.out.print(t);
+    }
+}
