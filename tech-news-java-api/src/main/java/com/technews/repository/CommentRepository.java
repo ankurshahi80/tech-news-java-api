2653efbfbc8dda4d59d7eaf3d4c5f8a1f323a1ea
ankurshahi80 <shahiankur80@gmail.com>
2022-01-20 23:19:53 -0500

    Configure the Data Source, Spring Session, and Port Number for the API

--- tech-news-java-api/src/main/java/com/technews/repository/CommentRepository.java
+++ tech-news-java-api/src/main/java/com/technews/repository/CommentRepository.java
@@ -1,2 +1,12 @@
-package com.technews.repository;public interface CommentRepository {
+package com.technews.repository;
+
+import com.technews.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository {
+    List<Comment> findAllCommentsByPostId(int postId);
 }
